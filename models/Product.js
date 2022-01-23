2c9f191b09d1206963df00fc167699177bc25617
johndavis <johndavis92790@gmail.com>
2022-01-23 16:49:32 -0700

    created models with columns, started on GET routes

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,33 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isNumeric: true
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,33 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    // include: [
+    //   {
+    //     model: Category,
+    //     attributes: ['id', 'category_name']
+    //   },
+      // {
+      //   model: ProductTag,
+      //   attributes: ['id', 'product_id', 'tag_id'],
+      //   include: {
+      //     model: Tag,
+      //     attributes: ['tag_name']
+      //   }
+      // },
+      // {
+      //   model: Tag,
+      //   attributes: ['tag_name'],
+      //   through: ProductTag,
+      //   as: 'tag_name'
+      // }
+    // ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
